aeab80f6ef4147b839b0d6a3aaef974c5a141c67
Syed Masood <84665073+syedsm@users.noreply.github.com>
2024-06-02 17:26:49 +0530

    Update apirouter.js

--- router/apirouter.js
+++ router/apirouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const apicontroller = require('../controller/apicontroller')
 
 router.get('/api/', apicontroller.rou)
+router.get('/api/h', apicontroller.heloroute)
 
 module.exports = router
