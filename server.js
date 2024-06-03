6b8db4a96959f85bdeabbebcc2f284116895bea7
Syed Masood <masoodsyed123786@gmail.com>
2024-06-03 11:16:01 +0530

    remove nodemuldes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 require('dotenv').config();
-const port=process.env.PORT || 5000
-const apirouter=require('./router/apirouter')
+const port = process.env.PORT || 5000
+const apirouter = require('./router/apirouter')
 
-app.use('/api/',apirouter)
-app.listen(port,()=>{
-    console.log("server is running");
+app.use('/api/', apirouter)
+app.listen(port, () => {
+    console.log(`server is running ${port}`);
 })
