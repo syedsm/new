49ddca6fd84d07bea70964f8367dcf40d8ef242c
Syed Masood <masoodsyed123786@gmail.com>
2024-06-03 15:49:23 +0530

    add

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 const apirouter = require('./router/apirouter')
 
-app.use('/api/', apirouter)
+app.use(apirouter)
 app.listen(port, () => {
     console.log(`server is running ${port}`);
 })
